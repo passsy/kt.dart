71c6eda5bbd8fe47b0cb08f64ce8b634ed885249
Pascal Welsch <pascal.welsch@gmail.com>
2018-11-21 01:01:25 +0100

    Allow optional message in exceptions

--- lib/src/exceptions.dart
+++ lib/src/exceptions.dart
@@ -1,10 +1,17 @@
 class IndexOutOfBoundsException implements Exception {
   final String message;
 
-  IndexOutOfBoundsException(this.message);
+  IndexOutOfBoundsException([this.message]);
 
   @override
-  String toString() => 'IndexOutOfBoundsException: $message';
+  String toString() => message == null ? 'IndexOutOfBoundsException' : 'IndexOutOfBoundsException: $message';
 }
 
-class NoSuchElementException implements Exception {}
+class NoSuchElementException implements Exception {
+  final String message;
+
+  NoSuchElementException([this.message]);
+
+  @override
+  String toString() => message == null ? 'NoSuchElementException' : 'NoSuchElementException: $message';
+}
