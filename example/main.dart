e10fbff61312fbd9281364a474281bd3a12ff85e
Pascal Welsch <pascal.welsch@gmail.com>
2018-11-28 17:20:43 +0100

    Improve example

--- example/main.dart
+++ example/main.dart
@@ -1,31 +1,61 @@
 import 'package:dart_kollection/dart_kollection.dart';
 
-import 'shop.dart';
-
 main() {
-  final rekasPrducts = getOrderedProducts(jbCustomers[reka]);
-  print("reka bought $rekasPrducts");
+  /**
+   * Lists
+   */
+  final numbers1 = listOf([1, 2, 3, 4]).map((it) => ">$it<");
+  print(numbers1); // [>1<, >2<, >3<, >4<]
 
-  final allOrdersOfJbShop = getAllOrderedProducts(jbShop);
-  var formattedSales = allOrdersOfJbShop
-      .map((it) => "Sold ${it.second}x '${it.first}', revenue ${it.third}\$")
-      .joinToString(separator: "\n");
-  print("total jbShop sales:\n${formattedSales}");
+  final numbers2 = listOf([1, 2, 3, 4]).flatMap((it) => listOf([it * 2, it * 3]));
+  print(numbers2); // [2, 3, 4, 6, 6, 9, 8, 12]
 
-  final revenue = allOrdersOfJbShop.map((it) => it.third).sum();
-  print("total jbShop revenue ${revenue}\$");
-}
+  final numbers3 = numbers2.filter((it) => it % 3 == 0);
+  print(numbers3); // [3, 6, 6, 9, 12]
 
-KSet<Product> getOrderedProducts(Customer customer) {
-  return customer.orders.flatMap((it) => it.products).toSet();
-}
+  final numbers4 = numbers3.distinct();
+  print(numbers4); //[3, 6, 9, 12]
 
-KList<KTriple<Product, int, int>> getAllOrderedProducts(Shop shop) {
-  return shop.customers
-      .flatMap((it) => getOrderedProducts(it))
-      .groupBy((it) => it)
-      .mapValues((entry) => entry.value.count())
-      .entries
-      .map((entry) => KTriple(entry.key, entry.value, (entry.value * entry.key.price).toInt()))
-      .sortedByDescending<num>((entry) => entry.third);
+  /**
+   * Better equals
+   */
+  final klistEquals = listOf([12, 9, 6, 3]) == listOf([12, 9, 6, 3]);
+  print(klistEquals); // true
+
+  final dartListEquals = [12, 9, 6, 3] == [12, 9, 6, 3];
+  print(dartListEquals); // false
+
+  final kMapEquals = mapOf({1: "Bulbasaur", 2: "Ivysaur"}) == mapOf({1: "Bulbasaur", 2: "Ivysaur"});
+  print(kMapEquals); // true
+
+  final dartMapEquals = {1: "Bulbasaur", 2: "Ivysaur"} == {1: "Bulbasaur", 2: "Ivysaur"};
+  print(dartMapEquals); // false
+
+  /**
+   * Sets
+   */
+  print(setOf([1, 1, 2, 2, 3])); // [1, 2, 3]
+
+  /**
+   * Maps
+   */
+  final pokemon = mutableMapOf({
+    1: "Bulbasaur",
+    2: "Ivysaur",
+  });
+  pokemon[1] = "Dito";
+  print(pokemon); // {1=Dito, 2=Ivysaur}
+
+  /**
+   * Tons of useful operators which *should* be part of the dart std lib
+   */
+  final numbers = listOf([1, 2, 3, 4]);
+  print(numbers.sum()); // 10
+
+  final numbers5 = listOf([1, 2, 3, 4]).sortedDescending();
+  print(numbers5); // [4, 3, 2, 1]
+
+  final beatles = setOf(["John", "Paul", "George", "Ringo"]);
+  print(beatles); // [John, Paul, George, Ringo]
+  print(beatles.joinToString(separator: "/")); // John/Paul/George/Ringo
 }
--- example/shop.dart
+++ example/shop.dart
@@ -1,5 +1,33 @@
 import 'package:dart_kollection/dart_kollection.dart';
 
+main() {
+  final rekasPrducts = getOrderedProducts(jbCustomers[reka]);
+  print("reka bought $rekasPrducts");
+
+  final allOrdersOfJbShop = getAllOrderedProducts(jbShop);
+  var formattedSales = allOrdersOfJbShop
+      .map((it) => "Sold ${it.second}x '${it.first}', revenue ${it.third}\$")
+      .joinToString(separator: "\n");
+  print("total jbShop sales:\n${formattedSales}");
+
+  final revenue = allOrdersOfJbShop.map((it) => it.third).sum();
+  print("total jbShop revenue ${revenue}\$");
+}
+
+KSet<Product> getOrderedProducts(Customer customer) {
+  return customer.orders.flatMap((it) => it.products).toSet();
+}
+
+KList<KTriple<Product, int, int>> getAllOrderedProducts(Shop shop) {
+  return shop.customers
+      .flatMap((it) => getOrderedProducts(it))
+      .groupBy((it) => it)
+      .mapValues((entry) => entry.value.count())
+      .entries
+      .map((entry) => KTriple(entry.key, entry.value, (entry.value * entry.key.price).toInt()))
+      .sortedByDescending<num>((entry) => entry.third);
+}
+
 class Shop {
   Shop(this.name, this.customers);
 
