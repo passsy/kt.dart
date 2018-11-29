060242d66791390dd9c557c6e28e1f3c1da96119
Pascal Welsch <pascal.welsch@gmail.com>
2018-11-29 16:04:11 +0100

    Move HTML report in its own dir
    HTML report wipes dir before  write

--- tool/run_coverage_locally.sh
+++ tool/run_coverage_locally.sh
@@ -5,4 +5,4 @@ pub global activate dart_codecov_generator
 pub global run coverage:collect_coverage --port=8111 -o out/coverage/coverage.json --resume-isolates --wait-paused &
 dart --observe=8111 test/dart_kollection_test.dart
 pub global run coverage:format_coverage --packages=.packages --report-on lib --in out/coverage/coverage.json --out out/coverage/lcov.info --lcov
-genhtml -o out/coverage out/coverage/lcov.info
+genhtml -o out/coverage/html out/coverage/lcov.info
