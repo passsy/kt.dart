686e5e88eb723f42caf50f154486b478d2f96228
Pascal Welsch <pascal.welsch@gmail.com>
2018-12-20 19:54:06 +0100

    Add more iterator tests

--- test/collection/iterator_test.dart
+++ test/collection/iterator_test.dart
@@ -6,6 +6,11 @@ import '../test/assert_dart.dart';
 
 void main() {
   group('InterOpKListIterator', () {
+    test("next at pos 0 is first element", () {
+      final i = InterOpKListIterator(["a", "b"], 0);
+      expect(i.next(), "a");
+    });
+
     test("hasNext true", () {
       final i = InterOpKListIterator(["a", "b"], 0);
       expect(i.hasNext(), isTrue);
@@ -16,6 +21,11 @@ void main() {
       expect(i.nextIndex(), 1);
     });
 
+    test("previousIndex when next exists", () {
+      final i = InterOpKListIterator(["a", "b"], 0);
+      expect(i.previousIndex(), -1);
+    });
+
     test("hasNext false", () {
       final i = InterOpKListIterator(["a", "b"], 2);
       expect(i.hasNext(), isFalse);
@@ -24,6 +34,11 @@ void main() {
     test("nextIndex returns list size when at end", () {
       final i = InterOpKListIterator(["a", "b"], 2);
       expect(i.nextIndex(), 2);
+    });
+
+    test("previousIndex when next exists", () {
+      final i = InterOpKListIterator(["a", "b"], 2);
+      expect(i.previousIndex(), 1);
     });
 
     test("start index has to be in range (smaller)", () {
@@ -39,6 +54,31 @@ void main() {
     test("start index has to be in range (larger)", () {
       final i = InterOpKListIterator(["a", "b"], 0);
       //for (var char in InterOpKIterator()) {}
+    });
+
+    test("remove is not implemented", () {
+      final i = InterOpKListIterator(["a", "b"], 0);
+      final e = catchException(() => i.remove());
+      expect(e, TypeMatcher<UnimplementedError>());
+    });
+
+    test("add adds item to underlying list", () {
+      var dartList = ["a", "b"];
+      final i = InterOpKListIterator(dartList, 0);
+      i.add("c");
+      expect(dartList, equals(["c", "a", "b"]));
+    });
+
+    test("set modifies current item", () {
+      var dartList = ["a", "b"];
+      final i = InterOpKListIterator(dartList, 0);
+      expect(i.next(), equals("a"));
+      i.set("x");
+      expect(dartList, equals(["x", "b"]));
+      expect(i.previous(), equals("x"));
+      expect(i.next(), equals("x"));
+      expect(i.next(), equals("b"));
+      expect(i.previous(), equals("b"));
     });
   });
 }
