8d3532c12322a9e829110fcd60ab670bd1732f55
Pascal Welsch <pascal.welsch@gmail.com>
2018-12-31 02:02:23 +0100

    Use varargs for mutableListOf and create mutableListFrom for Iterable

--- lib/src/collections.dart
+++ lib/src/collections.dart
@@ -67,7 +67,45 @@ KList<T> emptyList<T>() => EmptyList<T>();
 /**
  * Returns an empty new [MutableList].
  */
-KMutableList<T> mutableListOf<T>([Iterable<T> elements = const []]) =>
+KMutableList<T> mutableListOf<T>(
+    [T arg0,
+    T arg1,
+    T arg2,
+    T arg3,
+    T arg4,
+    T arg5,
+    T arg6,
+    T arg7,
+    T arg8,
+    T arg9]) {
+  List<T> args;
+  if (arg9 != null) {
+    args = [arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9];
+  } else if (arg8 != null) {
+    args = [arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8];
+  } else if (arg7 != null) {
+    args = [arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7];
+  } else if (arg6 != null) {
+    args = [arg0, arg1, arg2, arg3, arg4, arg5, arg6];
+  } else if (arg5 != null) {
+    args = [arg0, arg1, arg2, arg3, arg4, arg5];
+  } else if (arg4 != null) {
+    args = [arg0, arg1, arg2, arg3, arg4];
+  } else if (arg3 != null) {
+    args = [arg0, arg1, arg2, arg3];
+  } else if (arg2 != null) {
+    args = [arg0, arg1, arg2];
+  } else if (arg1 != null) {
+    args = [arg0, arg1];
+  } else if (arg0 != null) {
+    args = [arg0];
+  } else {
+    args = [];
+  }
+  return DartMutableList(args);
+}
+
+KMutableList<T> mutableListFrom<T>([Iterable<T> elements = const []]) =>
     DartMutableList(elements);
 
 /**
