35379549a8d728b1b96de9e055e207099330e895
Pascal Welsch <pascal.welsch@gmail.com>
2019-01-02 18:14:53 +0100

    Test annotations

--- test/dart_kollection_test.dart
+++ test/dart_kollection_test.dart
@@ -18,6 +18,7 @@ import 'collections_test.dart' as collections_test;
 import 'comparisons_test.dart' as comparisons_test;
 import 'exceptions_test.dart' as exceptions_test;
 import 'tuples_test.dart' as tuples_test;
+import 'util/annotation_test.dart' as annotation_test;
 import 'util/hash_test.dart' as hash_test;
 
 void main() {
@@ -39,5 +40,6 @@ void main() {
   comparisons_test.main();
   exceptions_test.main();
   tuples_test.main();
+  annotation_test.main();
   hash_test.main();
 }
