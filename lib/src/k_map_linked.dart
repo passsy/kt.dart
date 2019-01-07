ce84416b9882f843a6a2ec18787853146eb43eb7
Pascal Welsch <pascal.welsch@grandcentrix.net>
2019-01-07 03:13:58 +0100

    Remove unused import

--- lib/src/k_map_linked.dart
+++ lib/src/k_map_linked.dart
@@ -1,5 +1,4 @@
 import 'package:dart_kollection/dart_kollection.dart';
-import 'package:dart_kollection/src/collection/map_hash.dart';
 import 'package:dart_kollection/src/collection/map_linked.dart';
 
 abstract class KLinkedMap<K, V> implements KMutableMap<K, V> {
