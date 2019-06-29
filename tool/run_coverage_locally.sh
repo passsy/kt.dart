2ed0264d98d084ce57f22f1cadb24d51932fbcf2
Pascal Welsch <pascal.welsch@gmail.com>
2019-06-29 15:58:37 +0200

    Add dart 2.4.0 to supported versions
    Run coverage only on dart 2.2

--- tool/run_coverage_locally.sh
+++ tool/run_coverage_locally.sh
@@ -7,5 +7,9 @@
 pub global run coverage:collect_coverage --port=8111 -o out/coverage/coverage.json --resume-isolates --wait-paused &
 dart --observe=8111 --enable-asserts test/kt_dart_test.dart
 pub global run coverage:format_coverage --packages=.packages --report-on lib --in out/coverage/coverage.json --out out/coverage/lcov.info --lcov
-genhtml -o out/coverage/html out/coverage/lcov.info
-echo "open coverage report $PWD/out/coverage/html/index.html"
+
+if type genhtml >/dev/null 2>&1; then
+ genhtml -o out/coverage/html out/coverage/lcov.info echo "open coverage report $PWD/out/coverage/html/index.html"
+else
+ echo "genhtml not installed, can't generate html coverage output"
+fi
