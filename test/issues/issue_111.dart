67865ac70c56281638b701f745f95720c3eb90e1
Pascal Welsch <pascal@welsch.dev>
2019-10-26 17:44:20 +0200

    At least assert something and not only check compilation

--- test/issues/issue_111.dart
+++ test/issues/issue_111.dart
@@ -4,6 +4,7 @@ import "package:test/test.dart";
 void main() {
   // https://github.com/passsy/kt.dart/issues/111
   test("issue #111", () {
-    listOf(1, 2, 3).sortedBy((n) => n);
+    final result = listOf(1, 2, 3).sortedBy((n) => n);
+    expect(result, listOf(1, 2, 3));
   });
 }
