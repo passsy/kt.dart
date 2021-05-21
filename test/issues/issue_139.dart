1a0234579b1e6b88c229d7f1c7064b92736bd060
Pascal Welsch <pascal@welsch.dev>
2021-05-21 15:45:30 +0200

    Cleanup sample code

--- test/issues/issue_139.dart
+++ test/issues/issue_139.dart
@@ -4,23 +4,25 @@ import "package:test/test.dart";
 void main() {
   // https://github.com/passsy/kt.dart/issues/139
   test("issue #139", () {
-    var usd = Currency(1, "USD");
-    var inr = Currency(2, "INR");
-    KtList<Currency> currency = listOf(usd, inr);
-    KtList<CurrencyConversionData> conversion =
+    final usd = Currency(1, "USD");
+    final inr = Currency(2, "INR");
+    final KtList<Currency> currency = listOf(usd, inr);
+    final KtList<CurrencyConversionData> conversion =
         listOf(CurrencyConversionData(1, 2));
-    KtMap<Currency, KtList<Currency>> conversions =
+    final KtMap<Currency, KtList<Currency>> conversions =
         _toCurrencyMap(conversion, currency);
 
     // CRASH HERE (type 'EmptyList<Currency>' is not a subtype of type 'KtMutableList<Currency>' of 'defaultValue')
-    final result = conversions.getOrDefault(Currency(3, "EUR"), KtList.empty());
+    final result =
+        conversions.getOrDefault(Currency(3, "EUR"), const KtList.empty());
     expect(result, emptyList());
   });
 }
 
 KtMap<Currency, KtList<Currency>> _toCurrencyMap(
     KtList<CurrencyConversionData> conversion, KtList<Currency> currency) {
-  KtMap<int, Currency> currencyMap = currency.associateBy((cur) => cur.id);
+  final KtMap<int, Currency> currencyMap =
+      currency.associateBy((cur) => cur.id);
   return conversion.groupByTransform(
     (conv) => currencyMap.get(conv.fromRef)!,
     (conv) => currencyMap.get(conv.toRef)!,
