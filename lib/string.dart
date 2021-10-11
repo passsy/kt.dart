bba15849f9e1de588e76fdfe8535307eefd15260
Marcio Kuroki <marciokuroki@gmail.com>
2021-10-11 11:21:55 -0300

    Including Comments (Documentation)

--- lib/string.dart
+++ lib/string.dart
@@ -2,6 +2,9 @@ import 'package:meta/meta.dart';
 
 extension StringExtension on String {
 
+  /// Returns a copy of this string having its first character replaced with the result of the specified transform, 
+  /// or the original string if it's empty.
+  /// transform - function that takes the first character and returns the result of the transform applied to the character.
   @pragma('vm:prefer-inline')
   @pragma('dart2js:tryInline')
   @experimental
