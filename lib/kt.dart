2406b02de0b22bf80400f0b625f0a7c439bbe085
Pascal Welsch <pascal@welsch.dev>
2021-10-15 14:01:39 +0200

    Move string extensions in string folder
    Use an name extension to make that specific extension hidable

--- lib/kt.dart
+++ lib/kt.dart
@@ -3,5 +3,5 @@ library kotlin_dart;
 export "package:kt_dart/annotation.dart";
 export "package:kt_dart/collection.dart";
 export "package:kt_dart/exception.dart";
-export 'package:kt_dart/src/string.dart';
+export 'package:kt_dart/src/string/string.dart';
 export "package:kt_dart/standard.dart";
--- lib/src/string/string.dart
+++ lib/src/string/string.dart
@@ -1,6 +1,6 @@
 import 'package:meta/meta.dart';
 
-extension StringExtension on String {
+extension StringReplaceFirstCharExtension on String {
   /// Returns a copy of this string having its first character replaced with the result of the specified transform,
   /// or the original string if it's empty.
   /// transform - function that takes the first character and returns the result of the transform applied to the character.
