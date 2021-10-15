77721acb2dcf095a4a810ed5aae7a06de990bc2a
Pascal Welsch <pascal@welsch.dev>
2021-10-15 13:52:34 +0200

    Move string.dart into src/

--- lib/kt.dart
+++ lib/kt.dart
@@ -4,4 +4,4 @@ export "package:kt_dart/annotation.dart";
 export "package:kt_dart/collection.dart";
 export "package:kt_dart/exception.dart";
 export "package:kt_dart/standard.dart";
-export "package:kt_dart/string.dart";
+export 'package:kt_dart/src/string.dart';
