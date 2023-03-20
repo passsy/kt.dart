1e9bd9c0e594855a2f5480f8c4b4b19fe1defb3d
Pascal Welsch <pascal@welsch.dev>
2023-03-20 14:05:42 +0100

    Promote experimental APIs to stable

--- lib/src/string/string.dart
+++ lib/src/string/string.dart
@@ -1,12 +1,9 @@
-import 'package:meta/meta.dart';
-
 extension StringReplaceFirstCharExtension on String {
   /// Returns a copy of this string having its first character replaced with the result of the specified transform,
   /// or the original string if it's empty.
   /// transform - function that takes the first character and returns the result of the transform applied to the character.
   @pragma('vm:prefer-inline')
   @pragma('dart2js:tryInline')
-  @experimental
   String replaceFirstChar(String Function(String) transform) {
     if (isNotEmpty) {
       return transform(this[0]) + substring(1);
--- lib/standard.dart
+++ lib/standard.dart
@@ -1,8 +1,6 @@
 // ignore_for_file: unnecessary_cast, (dart 2.6 need some help in extensions)
-import 'package:meta/meta.dart' show experimental;
 
 /// An exception is thrown to indicate that a method body remains to be implemented.
-@experimental
 class NotImplementedException implements Exception {
   NotImplementedException([String? message])
       : message = message ?? "An operation is not implemented.";
@@ -16,11 +14,9 @@ class NotImplementedException implements Exception {
 /// Always throws [NotImplementedException] stating that operation is not implemented.
 @pragma('vm:prefer-inline')
 @pragma('dart2js:tryInline')
-@experimental
 // ignore: non_constant_identifier_names
 Never TODO([String? reason]) => throw NotImplementedException(reason);
 
-@experimental
 extension StandardKt<T> on T {
   /// Calls the specified function [block] with `this` value as its argument and returns its result.
   @pragma('vm:prefer-inline')
@@ -32,7 +28,6 @@ extension StandardKt<T> on T {
   /// Calls the specified function [block] with `this` value as its argument and returns `this` value.
   @pragma('vm:prefer-inline')
   @pragma('dart2js:tryInline')
-  @experimental
   T also(void Function(T) block) {
     block(this as T);
     return this as T;
@@ -41,7 +36,6 @@ extension StandardKt<T> on T {
   /// Returns `this` value if it satisfies the given [predicate] or `null`, if it doesn't.
   @pragma('vm:prefer-inline')
   @pragma('dart2js:tryInline')
-  @experimental
   T? takeIf(bool Function(T) predicate) {
     if (predicate(this as T)) return this as T;
     return null;
@@ -50,7 +44,6 @@ extension StandardKt<T> on T {
   /// Returns `this` value if it _does not_ satisfy the given [predicate] or `null`, if it does.
   @pragma('vm:prefer-inline')
   @pragma('dart2js:tryInline')
-  @experimental
   T? takeUnless(bool Function(T) predicate) {
     if (!predicate(this as T)) return this as T;
     return null;
@@ -62,7 +55,6 @@ extension StandardKt<T> on T {
 /// A zero-based index of current iteration is passed as a parameter to [action].
 @pragma('vm:prefer-inline')
 @pragma('dart2js:tryInline')
-@experimental
 void repeat(int times, void Function(int) action) {
   for (var i = 0; i < times; i++) {
     action(i);
